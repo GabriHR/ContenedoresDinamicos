24ad80968910a8172b219ad337fb7f5934b74de4
GabriHR <ghernrod@myuax.com>
2023-11-29 18:43:16 +0100

    Definición del vector utilizado en el bucle posteriormente.

---
+++ Ejercicio1/Vector_Iteradores.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> vec;
+
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+    vec.push_back(40);
+    vec.push_back(50);
+
+    vector<int>::iterator it;
+
+
+}

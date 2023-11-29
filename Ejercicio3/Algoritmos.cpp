2c6e36b9415a07503672c1e6f32ec8b573711102
GabriHR <ghernrod@myuax.com>
2023-11-29 18:56:11 +0100

    Definición del vector y sus enteros y la función que lo ordena de principio a fin de menor a mayor.

---
+++ Ejercicio3/Algoritmos.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<int> vec = {30, 10, 50, 20, 40};
+
+    sort(vec.begin(), vec.end());
+
+}

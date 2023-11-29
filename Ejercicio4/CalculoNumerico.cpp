dde59807d5e7a63a0365f39629fe191cc04f9c9b
GabriHR <ghernrod@myuax.com>
2023-11-29 19:13:31 +0100

    Utilización de diversas operaciones matemáticas con dichos valarrays e impresión de los diferentes resultados.

--- Ejercicio4/CalculoNumerico.cpp
+++ Ejercicio4/CalculoNumerico.cpp
@@ -13,5 +13,10 @@ int main() {
     valarray<double> valArr = {10.5, 20.5, 30.5, 40.5, 50.5};
     valarray<double> valArr2 = {1.5, 2.5, 3.5, 4.5, 5.5};
 
+    printResult(valArr + valArr2, "suma de los elementos: ");
+    printResult(valArr - valArr2, "resta de los elementos: ");
+    printResult(valArr * valArr2, "multiplicacion de los elementos: ");
+    printResult(valArr / valArr2, "division de los elementos: ");
 
+    return 0;
 }

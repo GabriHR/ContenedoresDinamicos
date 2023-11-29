1b7b094e81f84ac0bf74b53f942eb8f0744e0cdb
GabriHR <ghernrod@myuax.com>
2023-11-29 18:51:48 +0100

    Definición del adaptador de secuencia stack.

---
+++ Ejercicio2/ContenedoresEstandar_Secuencias.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<double> stk;
+
+    stk.push(1.1);
+    stk.push(2.2);
+    stk.push(3.3);
+    stk.push(4.4);
+    stk.push(5.5);
+    stk.push(6.6);
+
+
+}
